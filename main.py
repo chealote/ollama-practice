cc7718bd1c16001dcfb5d0cbe8b92365f951c862
Alejandro Lazarte <chealote@gmail.com>
2025-01-04 15:14:32 -0300

    'Update main.py with adding additional functionality and reformatting code'
    This commit was done by running the script

--- main.py
+++ main.py
@@ -4,7 +4,34 @@ import sys
 import subprocess
 import re
 
-url = "http://localhost:11434/api/chat"
+# options: [ yes, no, ... ]
+# answers: 0 1 2
+def get_option(options=["yes", "no"]):
+    prompt = "> "
+    option = input(prompt).lower()
+    while True:
+        try:
+            index = options.index(option)
+            break
+        except ValueError:
+            print("Please answer any of these:", options)
+            option = input(prompt).lower()
+
+    return options.index(option)
+
+def get_suggestion(payload):
+    url = "http://localhost:11434/api/chat"
+    try:
+        response = requests.post(url, json=payload, stream=True)
+    except Exception as e:
+        print("ERROR: the AI failed to reply:", e)
+        sys.exit(1)
+    suggested_command = ""
+    for line in response.iter_lines():
+        if line:
+            j = json.loads(line)
+            suggested_command += j["message"]["content"]
+    return suggested_command
 
 payload = {
         "model": "llama3.2:1b",
@@ -43,17 +70,50 @@ payload = {
         }
 
 # TODO ask to run `git diff`
-output = subprocess.run(["git", "diff"], stdout=subprocess.PIPE)
+init_command_diff = "git diff --cached"
+
+print("Run the following? (yes/no):", init_command_diff)
+
+if get_option() != 0:
+    print("You have to answer YES!")
+    sys.exit(1)
+
+output = subprocess.run(init_command_diff.split(" "), stdout=subprocess.PIPE)
 
 payload["messages"].append({
     "role": "user",
     "content": output.stdout.decode("utf-8"),
     })
 
-response = requests.post(url, json=payload, stream=True)
-for line in response.iter_lines():
-    if line:
-        j = json.loads(line)
-        sys.stdout.write(j["message"]["content"])
+suggested_command = get_suggestion(payload)
+
+print("")
+print("The AI has spoken, it suggests the following command:")
+print(suggested_command)
+print("Will you run it? (yes/no/other)")
+option = get_option(["yes", "no", "other"])
+
+while option != 0:
+    if option == 1:
+        print("The AI will remember that...")
+        sys.exit(1)
+
+    if option == 2:
+        suggested_command = get_suggestion(payload)
+        print("Here's another suggestion:")
+        print(suggested_command)
+        print("Will you run it? (yes/no/other)")
+
+    option = get_option(["yes", "no", "other"])
+    print("You answer:", option, "and the sugg command is:", suggested_command)
+
+
+commit_msg = re.findall("'[^']+'$", suggested_command)[0]
+commit_cmd = re.findall("^[^']+", suggested_command)[0]
+full_commit_cmd = commit_cmd.split(" ")
+full_commit_cmd.append(commit_msg)
+
+output = subprocess.run(full_commit_cmd, stdout=subprocess.PIPE)
+print(output)
 
 # TODO ask to run the full commit command
